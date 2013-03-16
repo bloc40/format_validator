73736687d836d8f57a65b002d61e07d8031dcdd1
Jamal El Milahi <jamal@elmilahi.com>
2013-03-16 12:37:01 -0700

    Move locale folder to locales

--- lib/format_validator.rb
+++ lib/format_validator.rb
@@ -5,5 +5,5 @@ require 'format_validator/zip_code_format_validator'
 require 'format_validator/future_date_validator'
 
 module FormatValidator
-  I18n.load_path << File.expand_path('../format_validator/locale/en.yml', __FILE__)
+  I18n.load_path << File.expand_path('../locales/en.yml', __FILE__)
 end
