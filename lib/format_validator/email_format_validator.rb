48f0b3adbe52178d80ce97c87c87f445a00e6a9b
Jamal El Milahi <jamal@elmilahi.com>
2014-03-18 07:26:27 -0700

    Treat the regex as strings

--- lib/format_validator/email_format_validator.rb
+++ lib/format_validator/email_format_validator.rb
@@ -2,7 +2,7 @@ module ActiveModel
   module Validations
     class EmailFormatValidator < ActiveModel::EachValidator
       def validate_each(object, attribute, value)
-        unless value =~ /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+        unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
           object.errors[attribute] << (options[:message] || I18n.t('form.errors.email'))
         end
       end
--- lib/format_validator/zip_code_format_validator.rb
+++ lib/format_validator/zip_code_format_validator.rb
@@ -2,7 +2,7 @@ module ActiveModel
   module Validations
     class ZipCodeFormatValidator < ActiveModel::EachValidator
       def validate_each(object, attribute, value)
-        unless value =~ /^\d{5}(-\d{4})?$/
+        unless value =~ /\A\d{5}(-\d{4})?\z/
           object.errors[attribute] << (options[:message] || I18n.t('form.errors.zip_code'))
         end
       end
