06b41691a40f8f09ccf122eeab1be9a1c0d96c2f
Jamal El Milahi <jamal@elmilahi.com>
2014-03-18 07:19:13 -0700

    Add validation for short format for zip code

--- lib/format_validator/zip_code_format_validator.rb
+++ lib/format_validator/zip_code_format_validator.rb
@@ -7,5 +7,13 @@ module ActiveModel
         end
       end
     end
+
+    class ZipCodeShortFormatValidator < ActiveModel::EachValidator
+      def validate_each(object, attribute, value)
+        unless value =~ /\A\d{5}\z/
+          object.errors[attribute] << (options[:message] || I18n.t('form.errors.zip_code'))
+        end
+      end
+    end
   end
 end
--- spec/format_validator/zip_code_format_validator_spec.rb
+++ spec/format_validator/zip_code_format_validator_spec.rb
@@ -34,3 +34,38 @@ describe 'ZipCodeFormatValidator' do
     end
   end
 end
+
+describe 'ZipCodeShortFormatValidator' do
+  describe '#validate_each' do
+    let(:klass) do
+      Class.new do
+        include ActiveModel::Validations
+        attr_accessor :zip_code
+        validates :zip_code, zip_code_short_format: true
+      end
+    end
+
+    subject { klass.new }
+
+    describe 'valid' do
+      %w[12345].each do |zip|
+        it 'should not add any error' do
+          subject.zip_code = zip
+          subject.valid?.must_equal true
+          subject.errors.messages.must_be_empty
+        end
+      end
+    end
+
+    describe 'invalid' do
+      ['123', '123456', '123 45', '1234X', '12345-1234', '12345-234X', '12345 6789'].each do |invalid_zip_code|
+        it "should add error when zip code is #{invalid_zip_code}" do
+          subject.zip_code = invalid_zip_code
+          subject.valid?.must_equal false
+          subject.errors.messages.size.must_equal 1
+          subject.errors.messages[:zip_code].must_equal ['is invalid']
+        end
+      end
+    end
+  end
+end
